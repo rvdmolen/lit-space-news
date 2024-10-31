b61b6903b8e6e0dd24d89a08e249d0bd3519ceb2
OR37FW <richard.van.der.molen@ing.com>
2024-10-31 09:22:50 +0100

    chore: sync

--- src/components/Header/Header.style.js
+++ src/components/Header/Header.style.js
@@ -49,7 +49,7 @@ export const HeaderStyle = css`
     border-color: #E2E8F0;
     outline-color: var(--blue);
     padding: 9px 4px 9px 40px;
-    background: transparent url("/assets/search-white.svg") no-repeat 13px;
+    background: transparent url("/assets/search.svg") no-repeat 13px;
   }
 
   .buttons {
@@ -67,9 +67,13 @@ export const HeaderStyle = css`
     }
   }
 
-  @media (prefers-color-scheme: light) {
+  .dark-mode input {
+    background: transparent url("/assets/search-white.svg") no-repeat 13px;
+  }
+
+  @media (prefers-color-scheme: dark) {
     input {
-      background: transparent url("/assets/search.svg") no-repeat 13px;
+      background: transparent url("/assets/search-white.svg") no-repeat 13px;
     }
   }
 `;
