a0ddaf774b4d39eab0a7153c5faa22b7eb685faf
OR37FW <richard.van.der.molen@ing.com>
2024-10-31 09:06:10 +0100

    chore: cleanup

--- src/app.js
+++ src/app.js
@@ -34,7 +34,6 @@ export class LitSpaceNews extends LitElement {
 
   render() {
     return html`
-
       <lit-space-news-header @search-change="${this.__handleSearchChange}"></lit-space-news-header>
       <main>
         <lit-space-news-banner></lit-space-news-banner>
